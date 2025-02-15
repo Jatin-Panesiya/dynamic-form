c6c27d23ba8957b23d34aaf17004d1363d919e4c
Jatin <jatinpanesiya2003@gmail.com>
2025-02-15 10:03:01 +0530

    fix : default step

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import AppContext from "./context/AppContext";
 
 const App = () => {
-  const [step, setStep] = useState(13);
+  const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({});
 
   return (
