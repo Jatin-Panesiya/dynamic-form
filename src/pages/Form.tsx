424c8dfe5c607a9576096b7ebfb6e2d239af8f12
Jatin <jatinpanesiya2003@gmail.com>
2025-02-16 17:48:07 +0530

    minor bug fix

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -4,13 +4,13 @@ const Form = () => {
   const component = useGetComponent();
 
   return (
-    <div>
+    <div className="root">
       <img
         src="./logo.png"
         className="w-[100px] h-[100px] absolute top-10 left-10"
         alt="Mantine logo"
       />
-      {component}
+      <div className="relative z-10">{component}</div>
     </div>
   );
 };
