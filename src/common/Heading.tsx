f99e5f5c50c13aea496ea773127c66c602a08d88
Jatin <jatinpanesiya2003@gmail.com>
2025-03-04 23:07:35 +0530

    fix : responsive bug

--- src/common/Heading.tsx
+++ src/common/Heading.tsx
@@ -1,6 +1,6 @@
 const Heading = ({ text }: { text: string }) => {
   return (
-    <div className="text-center max-[450px]:text-lg text-2xl font-bold p-5 max-[450px]:px-0 max-[450px]:py-2">
+    <div className="text-center max-[450px]:text-lg text-2xl font-bold p-5 max-[450px]:p-2">
       {text}
     </div>
   );
