7c29044c6f268d4277566a4556645e65e85a4092
Jatin <jatinpanesiya2003@gmail.com>
2025-03-15 10:08:42 +0530

    implement session storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { Navigate, Route, Routes } from "react-router";
 import Form from "./pages/Form";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AppContext from "./context/AppContext";
 import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PaymentSuccess from "./pages/PaymentSuccess";
 
 const App = () => {
-  const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState({});
+  const [step, setStep] = useState(() => {
+    return Number(sessionStorage.getItem("step")) || 1;
+  });
+
+  const [formData, setFormData] = useState(() => {
+    const savedFormData = sessionStorage.getItem("formData");
+    return savedFormData ? JSON.parse(savedFormData) : {};
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem("step", String(step));
+  }, [step]);
+
+  useEffect(() => {
+    sessionStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
 
   return (
     <AppContext.Provider value={{ step, setStep, formData, setFormData }}>
